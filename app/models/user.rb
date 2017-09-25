5f3df0b86c71ebdbb8f0c5ce95a3db37a256a12f
Marti Lloveras <marti-nini@hotmail.com>
2017-09-25 19:59:15 +0200

    Bug search start

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  
+  # Encrypt password
+  has_secure_password
+  
+  has_many :todos, foreign_key: :created_by
+  
+  validates_presence_of :name, :email, :password_digest
+  
 end
